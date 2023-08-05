0c22401d4c3f972607e1c1ebff7d65bea7e632b9
Zhima-Mochi <yehgoter@gmail.com>
2023-08-05 15:18:27 +0800

    update

--- example/main.go
+++ example/main.go
@@ -12,29 +12,39 @@ func main() {
 	queryOptions := []newsapi.QueryOption{}
 	queryOptions = append(queryOptions, newsapi.WithLanguage(newsapi.LanguageChineseTraditional))
 	queryOptions = append(queryOptions, newsapi.WithLocation(newsapi.LocationTaiwan))
-	queryOptions = append(queryOptions, newsapi.WithLimit(10))
+	queryOptions = append(queryOptions, newsapi.WithLimit(3))
 	endDate := time.Now()
-	startDate := endDate.Add(-time.Hour * 24)
+	startDate := endDate.Add(-time.Hour * 72)
 	queryOptions = append(queryOptions, newsapi.WithStartDate(startDate))
 	queryOptions = append(queryOptions, newsapi.WithEndDate(endDate))
 	// queryOptions = append(queryOptions, newsapi.WithPeriod(time.Hour))
 	handler.SetQueryOptions(queryOptions...)
 
-	newsList, err := handler.SearchNews("thread instagram")
+	newsList, err := handler.GetTopNews()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	newsapi.ToSourceLinks(newsList)
+	newsapi.FetchSourceLinks(newsList)
 	for _, news := range newsList {
 		fmt.Println("=================================")
 		fmt.Println(news.Title)
 		fmt.Println(news.Link)
-		news.Content, err = newsapi.FetchNewsContent(news.Link)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		fmt.Println(news.Content)
+		// news.Content, err = newsapi.FetchNewsContent(news.Link)
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	continue
+		// }
+		newsapi.FetchSourceContents([]*newsapi.News{news})
+		// fmt.Println(news.Content)
+		fmt.Println(news.SourceLink)
+		fmt.Println(news.SourceTitle)
+		fmt.Println(news.SourceImageURL)
+		fmt.Println(news.SourceImageWidth)
+		fmt.Println(news.SourceImageHeight)
+		fmt.Println(news.SourceDescription)
+		fmt.Println(news.SourceKeywords)
+		fmt.Println(news.SourceSiteName)
+		fmt.Println(news.SourceContent)
 	}
 }
